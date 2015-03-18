3ae4be0c7fef68827d86eea6f21c1bc12e22b676
dajvido <jedynywierny@gmail.com>
2015-03-18 09:44:31 +0100

    refactorization exercise1 from Lesson4

--- Lesson4/code/app.js
+++ Lesson4/code/app.js
@@ -1,17 +1,15 @@
 window.addEventListener('DOMContentLoaded', function () {
 	//When the page structure is loaded...
-    function getValue(input) {
-        return input.querySelector('input').value;
-    };
-    
+
     function setBmi(weight, height) {
-        bmi.innerText = Math.round((weight / (height/100 * height/100)) * 100) / 100;
+        var value = Math.round((weight / (height/100 * height/100)) * 100) / 100;
+        bmi.textContent = value;
         
-        if (19 < bmi.innerText && bmi.innerText < 25) {
+        if (19 < value && value < 25) { // textvalue
             bmi.classList.remove('medium');
             bmi.classList.remove('bad');
             bmi.classList.add('ok');
-        } else if (17 < bmi.innerText && bmi.innerText < 30) {
+        } else if (17 < value && value < 30) {
             bmi.classList.remove('ok');
             bmi.classList.remove('bad');
             bmi.classList.add('medium');
@@ -22,29 +20,29 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     };
     
-    var weight_input = document.querySelector('.weight'),
-        weight_val = weight_input.querySelector('span'),
-        height_input = document.querySelector('.height'),
-        height_val = height_input.querySelector('span'),
-        bmi = document.querySelector('h1 span'),
-        weight = getValue(weight_input),
-        height = getValue(height_input);
+    var weight_div = document.querySelector('.weight'),
+        weight_input = weight_div.querySelector('input'),
+        weight_val = weight_div.querySelector('span'),
+        height_div = document.querySelector('.height'),
+        height_input = height_div.querySelector('input'),
+        height_val = height_div.querySelector('span'),
+        bmi = document.querySelector('h1 span');
 
-        setBmi(weight, height);
+    setBmi(weight_input.value, height_input.value);
     
     weight_input.addEventListener('change', function() {
-        var weight = getValue(weight_input),
-            height = getValue(height_input);
+        var weight = weight_input.value,
+            height = height_input.value;
 
-        weight_val.innerText = weight;
+        weight_val.textContent = weight;
         setBmi(weight, height);
     });
     
     height_input.addEventListener('change', function() {
-        var weight = getValue(weight_input),
-            height = getValue(height_input);
-        
-        height_val.innerText = height;
+        var weight = weight_input.value,
+            height = height_input.value;
+
+        height_val.textContent = height;
         setBmi(weight, height);        
     });
 });
