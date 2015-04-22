5407d051988463fe2f242f53f8572fa80c414d38
dajvido <dawid.jaskot@gmail.com>
2015-04-22 21:08:04 +0200

    Delete comments

--- Lesson6/js/controllers/list_controller.js
+++ Lesson6/js/controllers/list_controller.js
@@ -7,9 +7,6 @@ function ListCtrl(listView, store) {
   });
   
   listView.on("todo_update", function (task_id) {
-//    var task = store.data[task_id];
-//    task.done = !task.done;
-//    listView.update(store.data);
     store.update(task_id, listView);
   });
 }
