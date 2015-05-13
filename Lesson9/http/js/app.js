a964c26df77b9871180c30b7e68a319dd5feaea0
dajvido <dawid.jaskot@gmail.com>
2015-05-13 09:49:08 +0200

    Add button for reset elements clicks counter

--- Lesson9/http/js/app.js
+++ Lesson9/http/js/app.js
@@ -9,7 +9,16 @@ angular.module('app', []).controller('AppCtrl', function ($scope, $http) {
 			.error(function(err) {
 				$scope.results = err;
 			});
-	}
+	};
+
+	$scope.resetClicks = function() {
+		$scope.results.forEach(function(element) {
+			$http.put('/elements/' + element.id, {count: 0})
+				.success(function(elem) {
+					element.count = elem.count;
+				})
+		});
+	};
 
 	$scope.clickElement = function (element) {
 		$http.put('/elements/' + element.id, {count: element.count + 1})
